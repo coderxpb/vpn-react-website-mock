d7baba4a96a391426adb10cd232c4d08e1702357
coderxpb <borapankaj901@gmail.com>
2021-09-04 16:07:56 +0530

    Change MainCards design and implementation

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -4,15 +4,18 @@ import './MainCard.scss';
 
 const MainCard = (props) =>{
 	return(
-		<div className="main-card">
-			<h1 className="main-card-heading">
-				{props.heading}
-			</h1>
-			<div className= "main-card-media">
-					<img src={props.img} alt={props.alt}/>
-					<p>{props.para}</p>
+		<div className={"main-card"+props.direction}>
+				<div className= {"main-card-media"+props.direction}>
+						<img src={props.img} alt={props.alt}/>
+						<div className="main-card-text">
+							<h1 className="main-card-heading">
+								{props.heading}
+							</h1>
+							<p>{props.para}</p>
+						</div>
+						
+				</div>
 			</div>
-		</div>
 	)
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,11 +3,13 @@ import {Link} from 'react-router-dom';
 import MainCard from '../MainCard';
 import './Home.scss';
 import homeImg from '../../images/home.svg';
+import wfaImg from '../../images/wfa.svg';
 
 const Home = () =>{
 	return(
 		<div className="home">
-			<MainCard para="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " heading="The most secure VPN." alt="hello3" img={homeImg}/>
+			<MainCard direction='' heading="The internet's most secure VPN." alt="hello3" img={homeImg} para="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " />
+			<MainCard direction='--reverse' heading="Never worry about your data again." alt="hello3" img={wfaImg} para="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " />
 		</div>
 	);
 }
