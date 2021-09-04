ba37e5cdb105aa1614d3be952bfc5f314128cf63
coderxpb <borapankaj901@gmail.com>
2021-09-04 18:35:45 +0530

    Add a responsive and well positioned footer

--- src/App.js
+++ src/App.js
@@ -9,12 +9,22 @@ function App() {
 		<Router>
 			<div className="App">
 
-			<Navbar />
-			<Switch>
-				<Route path='/' exact component={Home}/>
-				<Route path='/pricing' exact component={Pricing}/>
-			</Switch>
-			
+				<Navbar />
+				<Switch>
+					<Route path='/' exact component={Home}/>
+					<Route path='/pricing' exact component={Pricing}/>
+				</Switch>
+
+				<div className="footer-filler">
+
+				</div>
+				<footer className="basic-footer">
+					<p className="me">Designed and developed by <a href="http://github.com/coderxpb">Pankaj Bora</a>.</p>
+					<div className="footer-license-container">
+						<p>Vectors under <a href="hhttps://undraw.co/license">license</a> from <a href="http://undraw.co">Undraw</a>.</p>
+						<p>Icons under <a href="https://boxicons.com/usage#license">license</a> from <a href="http://boxicons.com">BoxIcons</a>.</p>
+					</div>
+				</footer>
 			</div>
 		</Router>  
   );
--- src/components/pages/Pricing.js
+++ src/components/pages/Pricing.js
@@ -7,7 +7,6 @@ const Pricing = () => {
 			<PricingCard features={['Single user login', 'Servers in 5 location', '500GB bandwidth', 'Basic cybersecurity']} tier="Individual" price="$0.99/m" />
 			<PricingCard features={['Upto 4 users','Servers in 15 location', '4TB bandwidth', 'Premium cybersecurity']} tier="Premium" price="$3.99/m" />
 			<PricingCard features={['Upto 20 users', 'Servers in 50 location', '20TB bandwidth', 'Enterprise cybersecurity']} tier="Enterprise" price="$10/m" />
-
 		</div>
 	);
 }
